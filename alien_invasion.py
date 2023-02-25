da87332c0cd4c8e31e66f52393ec2e0c49c0ac6e
andersonneedham <anderson_needham1@baylor.edu>
2023-02-25 13:20:48 -0500

    deleted draw aliens/ need to fix adding aliens to main invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,7 +5,6 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
-from alien import Alien
 
 
 class AlienInvasion:
@@ -24,9 +23,6 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
-        self.aliens = pygame.sprite.Group()
-
-        self._create_fleet()
 
         self.bg_color = (230, 230, 230)
 
@@ -88,19 +84,12 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
-    def _create_fleet(self):
-        # create fleet of aliens
-        # make an alien
-        alien = Alien(self)
-        self.aliens.add(alien)
-
     def _update_screen(self):
         # update images on the screen, and fkip to the new screen
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
-        self.aliens.draw(self.screen)
 
         # make the most recently drawn screen visible
         pygame.display.flip()
