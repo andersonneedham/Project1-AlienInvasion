341fcc5ce800c18d001caedd36a6635ff7e0712a
andersonneedham <anderson_needham1@baylor.edu>
2023-02-24 23:45:22 -0500

    added bounds for the ship to stay in screen

--- ship.py
+++ ship.py
@@ -26,9 +26,12 @@ class Ship:
 
     def update(self):
         # update the ships position based on the movement flag
-        if self.moving_right:
+
+        # update the ships x value, not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # update rect object from self.x
