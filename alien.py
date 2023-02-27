4595534e2af665e2d0aca99b1a4cd2e19bab30ab
andersonneedham <anderson_needham1@baylor.edu>
2023-02-27 10:26:35 -0600

    made alien appear on screen, typo in alien class

--- alien.py
+++ alien.py
@@ -5,7 +5,7 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     # a class to represent a single alien in the fleet
 
-    def _init__(self, ai_game):
+    def __init__(self, ai_game):
         # initialize the alien and set its starting position
         super().__init__()
         self.screen = ai_game.screen
--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,10 +24,12 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
 
-        self.bg_color = (230, 230, 230)
+        self._create_fleet()
 
         # set the background color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         # start the main loop for the game
@@ -84,12 +87,19 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _create_fleet(self):
+        # create a fleet of aliens
+        # make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     def _update_screen(self):
         # update images on the screen, and fkip to the new screen
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         # make the most recently drawn screen visible
         pygame.display.flip()
