069cf04bc5774b172befb29135cc1ae2052660e5
andersonneedham <anderson_needham1@baylor.edu>
2023-03-01 20:43:36 -0600

    displaying the level to screen based on level of game

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,6 +85,10 @@ class AlienInvasion:
             self._create_fleet()
             self.settings.increase_speed()
 
+            # increase level
+            self.stats.level += 1
+            self.sb.prep_level()
+
     def _update_aliens(self):
         # check if the fleet is at an edge, then update the positions of all aliens in the fleet
         self._check_fleet_edges()
@@ -146,6 +150,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # get rid of remaining aliens and bullets
             self.aliens.empty()
--- scoreboard.py
+++ scoreboard.py
@@ -65,6 +65,7 @@ class Scoreboard:
             self.prep_high_score()
 
     def show_score(self):
-        # draw score to the screen
+        # draw score and level to the screen
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
