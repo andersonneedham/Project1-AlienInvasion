edfcf582b1d30b8042ba4093e81f84fabe602985
andersonneedham <anderson_needham1@baylor.edu>
2023-03-01 20:27:30 -0600

    displaying high scores to the screen

--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,20 @@ class Scoreboard:
 
         # prepare the initial score image.
         self.prep_score()
+        self.prep_high_score()
+
+    def prep_high_score(self):
+        # turn the high score into a rendered image
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # center the high score at the top of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
     def prep_score(self):
         # turn the score into a rendered image
