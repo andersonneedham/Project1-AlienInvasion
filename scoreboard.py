5d0633245f2659a41249028ce74ba7a94077296e
andersonneedham <anderson_needham1@baylor.edu>
2023-03-01 19:02:40 -0600

    creater scoreboard and the display image

--- scoreboard.py
+++ scoreboard.py
@@ -17,3 +17,15 @@ class Scoreboard:
 
         # prepare the initial score image.
         self.prep_score()
+
+    def prep_score(self):
+        # turn the score into a rendered image
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(
+            score_str, True, self.text_color, self.settings.bg_color
+        )
+
+        # display the score at the top right of the screen
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
